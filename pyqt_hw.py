08d3ffc0bda5cdef1a913dd8138fd98deb5e095c
René Fritze <rene.fritze@wwu.de>
2020-11-19 10:20:28 +0100

    fixup! fixup! qt hw

--- pyqt_hw.py
+++ pyqt_hw.py
@@ -1,6 +1,13 @@
-from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+class MyWindow(QtWidgets.QMainWindow):
+    """Auto-closing window"""
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        QtCore.QTimer.singleShot(3000, self.close)
+
 def test_main():
-    app = QApplication([])
-    label = QLabel('Hello World!')
-    label.show()
-    app.exec_()
+    app = QtWidgets.QApplication(sys.argv)
+    my_win = MyWindow()
+    my_win.show()
+    sys.exit(app.exec_())
